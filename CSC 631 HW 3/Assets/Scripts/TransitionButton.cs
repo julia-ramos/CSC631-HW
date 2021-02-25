4da4d17f0fff1a665a8cbf08c54fdc0c781c56d2
Julia Beatriz Ramos <julia.beatriz99@yahoo.com>
2021-02-25 14:32:37 -0800

    Renamed project folder to CSC 631 HW 3

---
+++ CSC 631 HW 3/Assets/Scripts/TransitionButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionButton : MonoBehaviour
+{
+    public void transition() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void goBack() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
---
+++ CSC 631 HW 3/Assets/Scripts/animationStateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWavingHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWavingHash = Animator.StringToHash("isWaving");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWaving = animator.GetBool(isWavingHash);
+        bool wPressed = Input.GetKey("w");
+
+        if(!isWaving && wPressed) {
+            animator.SetBool(isWavingHash, true);
+        }
+        if(isWaving && !wPressed) {
+            animator.SetBool(isWavingHash, false);
+        }
+    }
+}
